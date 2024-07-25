10ebf9e55d8b5fea3ce3c8b27504db7ef57448b2
tomparson-ada <tomparson@ada.ac.uk>
2024-07-25 13:23:01 +0000

    Remove distractions, add comments

--- src/app/components/FaveMovieAdder.jsx
+++ src/app/components/FaveMovieAdder.jsx
@@ -1,16 +1,20 @@
-
-// saveToWatchList is passed to FaveMovieAdder from parent component
+// Our saveToWatchList function is passed to this FaveMovieAdder component from its parent component
 export default function FaveMovieAdder({saveToWatchList}) {
 
+    // Set up a function for us to handle data submitted from our simple form
     function handleSubmit(formData) {
+        // Get the movie name from whatever the user entered into the 'new-movie' input
         const movie = formData.get('new-movie');
-        console.log(movie,'<--- movie');
-
+        // Log this to the console
+        console.log(movie,'<--- new movie to add to watchlist');
+        // We can run saveToWatchList here, which has been passed through on line 2
+        // Running this will trigger an update to the state of the parent component, so the movie list will
+        // update in our interface
         saveToWatchList(movie);
-        // call to saveToWatchList defined in the parent component will trigger an update to state at the parent level
     }
+    // Return some form HTML
     return <form action={handleSubmit}>
-        <input style={{color: 'black'}}  name="new-movie" type="text"></input>
+        <input name="new-movie" type="text"></input>
         <button type="submit">Add to watch list</button>
     </form>
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
+// By default components are Server Components, meaning they are rendered on the server
+// We need this component to be a Client Component, meaning it is rendered in the browser, so we can access LocalStorage in the browser
+// To do this, we use 'use client' at the top of the file
+
 'use client'
 
 import { useState, useEffect } from 'react';
@@ -5,8 +9,21 @@ import FaveMovieAdder from './components/FaveMovieAdder';
 
 export default function Home() {
 
+  // Below, we use localStorage in the browser to store key-value pairs of data in the user's browser
+  // More info on localStorage: https://www.w3schools.com/jsref/prop_win_localstorage.asp
+  // How to view localStorage data in Chrome Web Inspector: https://developer.chrome.com/docs/devtools/storage/localstorage/
+
+  // useState is a React hook that lets us keep the value of variables "in state", meaning
+  // any changes are synced across different places the variable is used
+  // useState returns 2 things - a variable and a 'setter' function that we use to change the value
+  // of the variable later
+  // https://react.dev/reference/react/useState
   const [watchList,setWatchList] = useState([]);
 
+  // useEffect is a React hook that lets us specify code that is run immediately after rendering a component
+  // (as a 'side effect' of the component being rendered)
+  // this lets us utilise data from the browser using localStorage only after the component has fully loaded
+  // https://react.dev/reference/react/useEffect
   useEffect(() => { 
     const data = localStorage.getItem('watchList');
    
@@ -15,20 +32,25 @@ export default function Home() {
     }
   },[])
 
+  // Here, we specify a function which saves a movie to our watchlist variable, and stores the new value in localStorage
   function saveToWatchList(movie) {
-    const data = localStorage.getItem('watchList');
-    const parsed = JSON.parse(data) || [];
-    parsed.push(movie);
-    localStorage.setItem('watchList',JSON.stringify(parsed));
-    // setWatchList is in scope! setWatchList can update state of Home component
-    setWatchList(parsed);
+    // Get the current watchlist from localStorage
+    const data = localStorage.getItem('watchList')
+    // Turn this JSON string into an object (it will be an array) or use an empty array otherwise
+    const parsed = JSON.parse(data) || []
+    // Add our new movie onto the array
+    parsed.push(movie)
+    // Update localStorage with a JSON encoded string of the new, parsed, object
+    localStorage.setItem('watchList',JSON.stringify(parsed))
+    // We can now use setWatchList, the 'setter' function given to us by useState, since this is in scope in this component
+    setWatchList(parsed)
   }
 
   return (
       <>
-      {/* pass saveToWatchList as a prop to FaveMovieAdder */}
-      <FaveMovieAdder saveToWatchList={saveToWatchList} />
-      {watchList.length ? <ul>{watchList.map(film => <li key={film}>{film}</li>)}</ul> : <p>No watch lists in local storage</p>}
+        {/* Pass saveToWatchList as a prop to FaveMovieAdder */}
+        <FaveMovieAdder saveToWatchList={saveToWatchList} />
+        {watchList.length ? <ul>{watchList.map(film => <li key={film}>{film}</li>)}</ul> : <p>No watch lists in local storage</p>}
       </>
   )
 }
